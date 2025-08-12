6ccf96036082914d5c162d6f50364e86537d695c
amitriptilin <zopabolsac@gmail.com>
2025-08-12 22:19:02 +0300

    Add whole portfolio

---
+++ yakrut/src/helpers/projectsList.js
@@ -0,0 +1,44 @@
+import project01 from './../img/projects/01.jpg'
+import project01Big from './../img/projects/01-big.jpg'
+
+import project02 from './../img/projects/02.jpg'
+import project02Big from './../img/projects/02-big.jpg'
+
+import project03 from './../img/projects/03.jpg'
+import project03Big from './../img/projects/03-big.jpg'
+
+import project04 from './../img/projects/04.jpg'
+import project04Big from './../img/projects/04-big.jpg'
+
+const projects = [
+    {   
+        title: 'Calculator on JavaScript',
+        skills: 'JavaScript, CSS, HTML',
+        img: project01,
+        imgBig: project01Big,
+        gitHubLink: 'https://www.aviasales.ru/',
+    },
+    {   
+        title: 'Value converter on JavaScript',
+        skills: 'JavaScript, CSS, HTML',
+        img: project02,
+        imgBig: project02Big,
+        gitHubLink: 'https://www.aviasales.ru/',
+    },
+    {   
+        title: 'Task list Telegram bot',
+        skills: 'Python, Asyncio, PostgreSQL',
+        img: project03,
+        imgBig: project03Big,
+        gitHubLink: 'https://www.aviasales.ru/',
+    },
+    {   
+        title: 'Internet magazine w/ admin panel',
+        skills: 'React, CSS, HTML, Node.Js, PostgreSQL',
+        img: project04,
+        imgBig: project04Big,
+    },
+
+];
+
+export {projects}
